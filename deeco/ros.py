01eb38a4e11413c81bbb8388e3c2d652487de064
Gabriel <gabriel.araujo.5000@gmail.com>
2021-12-17 08:22:45 -0300

    Add AliveComponent and Manager for deeco to be used inside ros

--- deeco/ros.py
+++ deeco/ros.py
@@ -1,7 +1,8 @@
 from deeco.core import Component
 import rclpy
 from rosgraph_msgs.msg import Clock
-from std_msgs.msg import Int64
+from std_msgs.msg import String, Int64
+from rclpy.node import Node as RosNode
 from deeco.sim import Sim, SimScheduler
 from time import sleep
 import threading
@@ -13,7 +14,6 @@ class ROSComponent(Component):
         self.node.runtime.add_ros_component(self)
         self.ros_attributes = {}
         self.ros_node = rclpy.create_node(f"ensemble_{topic}")
-
 
 class ROSScheduler(SimScheduler):
     def __init__(self):
@@ -75,3 +75,51 @@ class ROSSim(Sim):
             pass
         print('All done')
         self.executor_thread.join()
+
+class AliveComponent(ROSComponent):
+    def __init__(self, name, status, node, topic):
+        super().__init__(node, topic)
+        self.name = name
+        self.status = status
+        self.last_heard = time.time()
+
+    def update_heard(self, new_timestamp):
+        self.last_heard = new_timestamp
+
+    def update_status(self, new_status):
+        self.status = new_status
+
+    def __contains__(self, item):
+        return self.name == item.name
+
+class Manager():
+    def __init__(self, node, alive_topic):
+        self.node = node
+        self.components = []
+        self.subscription = self.create_subscription(
+            String,
+            alive_topic,
+            self.alive_clbk,
+            10)
+        self.subscription  # prevent unused variable warning
+        self.check_period = 5000
+        self.timer1 = self.create_timer(self.check_period, self.check_alive)
+
+    def check_alive(self):
+        for comp in self.components:
+            # get ros time
+            if time.time() < comp.last_heard + self.check_period:
+                comp.update_status('disconnected')
+
+    def alive_clbk(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+        comp_name = json.loads(msg.data)['alive']
+        component = AliveComponent(comp_name, 'alive', self.node, f'{comp_name}/knowledge')
+        if component not in self.components:
+            self.components.append(component)
+        else:
+            idx = self.components.index(self.components)
+            self.components[idx].update_heard(time.time())
+            if not self.components[idx].status == 'alive':
+                self.components[idx].update_status('alive')
+>>>>>>> Add AliveComponent and Manager for deeco to be used inside ros
